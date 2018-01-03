c8260639dab6dd40c2e5eac6a952f5064d0b432e
LED <915794378@qq.com>
2018-01-03 13:37:22 +0800

    ！！

--- app/src/main/java/com/example/windkts/final_project/CollectedFragment.java
+++ app/src/main/java/com/example/windkts/final_project/CollectedFragment.java
@@ -1,19 +1,107 @@
 package com.example.windkts.final_project;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CollectedFragment extends Fragment {
-
+    private View view=null;
+    private RecyclerView recyclerview;
+    private  RvAdapter mAdapter ;
+    private HistoryOp historyOp;
+    private List<History> history = new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_collected, container, false);
 
-        return inflater.inflate(R.layout.fragment_collected, container, false);
+        recyclerview = (RecyclerView) view.findViewById(R.id.recyclerview);
+        setupRecyclerView(recyclerview);
+        return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        update();
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.onHiddenChanged(isVisibleToUser);
+        if (isVisibleToUser) {   // 在最前端显示 相当于调用了onResume();
+            update();
+        }
+    }
+    private void update(){
+        historyOp = new HistoryOp(getActivity());
+
+        List<History> newData = historyOp.getAllLiked();
+        history.clear();
+        history.addAll(newData);
+        Log.e("Frag","history: "+ String.valueOf(history.size()));
+        if(mAdapter!=null){
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+    private void setupRecyclerView(final RecyclerView recyclerView) {
+
+        mAdapter = new RvAdapter <History>(getActivity(), R.layout.collected_item, history) {
+            @Override
+            public void convert(ViewHolder holder, History h) {
+                TextView source = holder.getView(R.id.source);
+                TextView result = holder.getView(R.id.result);
+                source.setText(h.getSource());
+                result.setText(h.getResult());
+                //to do ...
+            }
+
+        };
+        mAdapter.setOnItemClickListener (new RvAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                Intent intent = new Intent(getContext(), DetailActivity.class);
+
+                History h = history.get(position);
+                intent.putExtra("history",h);
+                getContext().startActivity(intent);
+
+            }
+            @Override
+            public void onLongClick(final int position) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(recyclerView.getContext());
+                builder.setTitle("取消收藏该人物？")
+                        .setNegativeButton("取消", null)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface DialogInterface, int i) {
+                                History h = history.get(position);
+                                h.setIs_liked(0);
+                                historyOp.upDataData(h);
+                                history.remove(position);
+                                mAdapter.notifyDataSetChanged();
+                            }
+                        });
+                builder.create().show();
+            }
+        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        recyclerView.setAdapter(mAdapter);
+    }
 
 }
--- app/src/main/java/com/example/windkts/final_project/HomeFragment.java
+++ app/src/main/java/com/example/windkts/final_project/HomeFragment.java
@@ -1,15 +1,27 @@
 package com.example.windkts.final_project;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.service.autofill.FillEventHistory;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by windtksLin on 2018/1/1 0001.
@@ -18,6 +30,10 @@ import android.widget.TextView;
 public class HomeFragment extends Fragment {
     private EditText input;
     private View view=null;
+    private RecyclerView recyclerView;
+    private  RvAdapter mAdapter ;
+    private HistoryOp historyOp;
+    private List<History> history = new ArrayList<>();
 
     private Button msource;
     private Button mtarget;
@@ -41,8 +57,8 @@ public class HomeFragment extends Fragment {
         input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(event != null && event.getKeyCode()== KeyEvent.KEYCODE_ENTER){
-                    if(event.getAction() == KeyEvent.ACTION_DOWN){
+                if(event!=null && event.getKeyCode()==KeyEvent.KEYCODE_ENTER){
+                    if(event.getAction()== KeyEvent.ACTION_UP){
                         Intent intent = new Intent(getContext(),TranslateActivity.class);
                         intent.putExtra("query",input.getText().toString());
                         //测试用
@@ -51,12 +67,74 @@ public class HomeFragment extends Fragment {
                         getContext().startActivity(intent);
                         return true;
                     }
+                    return true;
                 }
                 return false;
             }
         });
 
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        setupRecyclerView(recyclerView);
+
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        HistoryOp = new HistoryOp(getActivity());
+        List<History> newData = historyOp.getAllData();
+        history.clear();
+        history.addAll(newData);
+
+        if(mAdapter!=null){
+            mAdapter.notifyDataSetChanged();
+        }
+
+    }
+
+    private void setupRecyclerView(final RecyclerView recyclerView) {
+
+        mAdapter = new RvAdapter <History>(getActivity(), R.layout.collected_item, history) {
+            @Override
+            public void convert(ViewHolder holder, History h) {
+                TextView source = holder.getView(R.id.source);
+                TextView result = holder.getView(R.id.result);
+                source.setText(h.getSource());
+                result.setText(h.getResult());
+                //to do ..
+            }
+
+        };
+
+        mAdapter.setOnItemClickListener (new RvAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                Intent intent = new Intent(getContext(), DetailActivity.class);
+                //to do ..
+                History h = history.get(position);
+                intent.putExtra("history",h);
+                getContext().startActivity(intent);
+
+            }
+            @Override
+            public void onLongClick(final int position) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(recyclerView.getContext());
+                builder.setTitle("删除人物？")
+                        .setNegativeButton("取消", null)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface DialogInterface, int i) {
+                                historyOp.deleteDataByName(history.get(position).getSource());
+                                history.remove(position);
+                                Log.e("heros","when delete: "+String.valueOf(history.size()));
+                                mAdapter.notifyDataSetChanged();
+                            }
+                        });
+                builder.create().show();
+            }
+        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        recyclerView.setAdapter(mAdapter);
     }
 
 
